19ef2d80ecc76684aa4394ff9a7b07e9c5467364
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-03 14:07:54 -0500

    ensure code passes pylint

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,3 +1,4 @@
+"""Module providing a function for sentiment analysis of provided text."""
 import requests
 import json
 
@@ -6,11 +7,13 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response)
-    print(formatted_response)
-
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
     return {'label': label, 'score': score}
 
-# sentiment_analyzer("I am really upset")
+# print(sentiment_analyzer("I am really upset"))
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
+"""Module handling the Flask HTTP server."""
+
 from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
@@ -9,15 +7,19 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    """Function handling sentiment analysis request."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is None:
+        return "Invalid input ! Try again."
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 
 @app.route("/")
 def render_index_page():
+    """Function handling root GET requests."""
     return render_template('index.html')
 
 if __name__ == "__main__":
